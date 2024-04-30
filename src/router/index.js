d3217e7a4eb17aff09aa64122d82004cdf832e7a
Cam1409 <eocanaba14@ucvvirtual.edu.pe>
2024-04-30 10:37:30 -0500

    Actualizaciones

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'LoginView',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login/LoginView.vue')
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/menu',
+    name: 'MenuView',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Menu/MenuView.vue')
   }
 ]
 
---
+++ src/views/Login/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+        <v-container class="cont1">
+          <v-col class="login-c1">
+            <p>
+              HOLA ESTAMOS VIVOS ESTA CORRIENDO EL FRONTED NOOOO
+            </p>
+            <v-text-field
+              label="Usuario"
+              prepend-icon="$vuetify"
+              variant="outlined"
+            ></v-text-field>
+          </v-col>
+        </v-container>
+  </v-container>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LoginView',
+});
+
+
+</script>
+
+<style src='../../views/Login/login.css'></style>
