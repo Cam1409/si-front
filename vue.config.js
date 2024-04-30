c4bb17a60848e1d2af5af7ed57e1644c2bdc7627
Cam1409 <eocanaba14@ucvvirtual.edu.pe>
2024-04-30 10:54:40 -0500

    actualizado-p

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  
+  devServer: {
+    port: 3004 // Puerto que deseas utilizar
+  },
 
   pluginOptions: {
     vuetify: {
